c9a6530f285fc3b5dc70f5ec0be421162370a69c
Anthony Marcar <anthony@Anthonys-MacBook-Pro.local>
2022-05-07 12:03:14 +1000

    add title/meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Georgina & Chris</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Georgina and Chris' Wedding" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Georgina and Chris' Wedding" />
       </Head>
       <body>
         <Main />
