91adfde5bd51f9c31a28194fd91d7612c8e4ad3b
Anthony Marcar <anthony@Anthonys-MacBook-Pro.local>
2022-06-06 20:25:18 +1000

    mindless fun

--- pages/things-to-know.js
+++ pages/things-to-know.js
@@ -9,41 +9,35 @@ function Page() {
       <h1>Things to know</h1>
       <section>
         <h2>COVID</h2>
-        <p>
-          This is what it will look like
-          empus.
-        </p>
+        <p>This is what it will look like empus.</p>
       </section>
       <section>
         <h2>Accommodation Options</h2>
         <p>
-        There is no accommodation at Boomrock or within immediate proximity to the venue. We recommend finding accommodation in central Wellington (try Airbnb, Booking.com) or Johnsonville. We don’t have any particular recommendations for places to stay but this guide to Wellington suburbs gives a sense of the different parts of town.   
+          There is no accommodation at Boomrock or within immediate proximity to
+          the venue. We recommend finding accommodation in central Wellington
+          (try Airbnb, Booking.com) or Johnsonville. We don’t have any
+          particular recommendations for places to stay but this guide to
+          Wellington suburbs gives a sense of the different parts of town.
         </p>
       </section>
       <section>
         <h2>Transport on the day</h2>
         <p>
-We will organise a shuttle bus to transport guests to and from the wedding venue. The bus will stop at two locations to collect guests and take them to the wedding venue. We kindly ask all guests to use the shuttle option, as there is very limited parking at the venue and roads are tricky. These locations and pick up times are:
-Wellington Central Railway Station (pick up time FILL IN); and
-Johnsonville Railway Station (pick up time FILL IN)
-At the end of the evening, buses will be leaving Boomrock to return guests to Wellington at two different times [what are these times?]
-          
+          We will organise a shuttle bus to transport guests to and from the
+          wedding venue. The bus will stop at two locations to collect guests
+          and take them to the wedding venue. We kindly ask all guests to use
+          the shuttle option, as there is very limited parking at the venue and
+          roads are tricky. These locations and pick up times are: Wellington
+          Central Railway Station (pick up time FILL IN); and Johnsonville
+          Railway Station (pick up time FILL IN) At the end of the evening,
+          buses will be leaving Boomrock to return guests to Wellington at two
+          different times [what are these times?]
         </p>
       </section>
       <section>
         <h2>Childcare</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-          euismod libero elit, at condimentum justo aliquet nec. Maecenas vel
-          orci eu tortor blandit placerat vehicula at elit. Morbi aliquet leo at
-          viverra luctus. Quisque vitae lectus vestibulum, venenatis orci quis,
-          pretium felis. Nam ultricies suscipit ligula. In convallis efficitur
-          consectetur. Pellentesque habitant morbi tristique senectus et netus
-          et malesuada fames ac turpis egestas. Duis enim lorem, imperdiet a
-          egestas eget, molestie vitae elit. Morbi nunc quam, sagittis sit amet
-          tempor a, varius ac mauris. Integer hendrerit ipsum vel tincidunt
-          tempus.
-        </p>
+        <p>Moo moo moo</p>
       </section>
       <section>
         <h2>Things to Do</h2>
