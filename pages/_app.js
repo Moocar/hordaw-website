9dcb25025d786352af9c53630011952489af9741
Anthony Marcar <anthony@Anthonys-MacBook-Pro.local>
2022-06-08 04:19:37 +1000

    address next warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles.css";
 import "../normalize.css";
 import Layout from "../components/layout";
@@ -5,8 +6,15 @@ import Layout from "../components/layout";
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Georgina and Chris' Wedding" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,9 +8,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charset="UTF-8" />
-        <meta name="description" content="Georgina and Chris' Wedding" />
       </Head>
       <body>
         <Main />
